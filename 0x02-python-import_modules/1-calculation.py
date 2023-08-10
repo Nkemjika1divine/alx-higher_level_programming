d9b312813428523a946336fbe704f6fcb07bca4f
Nkemjika1divine <cndivine@gmail.com>
2023-08-10 03:59:54 -0700

    import and modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-a = 10
-b = 5
-print(f"{a:d} + {b:d} = {add(a, b):d}")
-print(f"{a:d} - {b:d} = {sub(a, b):d}")
-print(f"{a:d} * {b:d} = {mul(a, b):d}")
-print(f"{a:d} / {b:d} = {div(a, b):d}")
+if __name__ == "__main__":
+    """This program pperforms arithmetic calculations of 10 and 5"""
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print(f"{a:d} + {b:d} = {add(a, b):d}")
+    print(f"{a:d} - {b:d} = {sub(a, b):d}")
+    print(f"{a:d} * {b:d} = {mul(a, b):d}")
+    print(f"{a:d} / {b:d} = {div(a, b):d}")
