c766fd3249dfc546c31a6ce440844b899f35e3ac
Nkemjika1divine <cndivine@gmail.com>
2023-09-11 10:24:06 -0700

    Python Inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This is a BaseGeometry class"""
+
+
+class BaseGeometry:
+    """initializing tge class"""
+
+    def area(self):
+        """This always raises an exceotion cos it is not imolemented"""
+        raise Exception("area() is not implemented")
