266aaa51769841a94b8de7a3f277f65d54eb7b7d
Nkemjika1divine <cndivine@gmail.com>
2023-10-17 18:47:19 -0700

    SQL intro

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- This creates a hbtn_0c_0 database in my server
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- This script deletes the database hbtn_0c_0 in my server.
+CREATE TABLE IF NOT EXISTS first_table (
+id INT,
+name VARCHAR(256)
+);
