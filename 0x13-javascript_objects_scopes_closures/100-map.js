0bdb2fa775f6ad7f9313e1735f8fe83bc2872715
Nkemjika1divine <cndivine@gmail.com>
2023-11-14 09:08:52 -0800

    Javascript object, scopes and ckosures

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list = require('./100-data').list; //.list accesses the list property from the object
+const list = require('./100-data').list; // .list accesses the list property from the object
 const map = list.map((x, i) => x * i);
 console.log(list);
 console.log(map);
