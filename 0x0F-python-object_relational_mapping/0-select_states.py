1d04e25a44cabb9610ff18b7b4d6edf8e4e71f63
Nkemjika1divine <cndivine@gmail.com>
2023-11-16 16:43:20 -0800

    Python Object Relational mapping

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This script lists all states from the database"""
+
+if __name__ = "__main__":
+    import MySQLdb
+    import sys
+
+    name = sys.argv[3]
+    uname = sys.argv[1]
+    pwd = sys.argv[2]
+    host = "localhost"
+    port = 3306
+    my_query = "SELECT * FROM states BY ORDER id ASC"
+
+    db = MySQLdb.connect(host=host, user=_name, passwd=pwd, db=name, port=port)
+
+    cursor = db.cursor(my_query)
+
+    complete_data = cursor.fetchall()
+
+    for data in complete_data:
+        print(data)
+
+    cursor.close()
+    db.close()
