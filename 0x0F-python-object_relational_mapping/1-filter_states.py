ee1127b6d745891332dc5655c9b1eb85843989ba
Nkemjika1divine <cndivine@gmail.com>
2023-11-17 01:44:17 -0800

    Python Object Relational mapping

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This script lists all states starting with "N" from the database"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    name = sys.argv[3]
+    uname = sys.argv[1]
+    pwd = sys.argv[2]
+    host = "localhost"
+    s_port = 3306
+    q = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+
+    db = MySQLdb.connect(
+        host=host, user=uname, passwd=pwd, db=name, port=s_port
+    )
+
+    cursor = db.cursor()
+    cursor.execute(q)
+
+    complete_data = cursor.fetchall()
+
+    for data in complete_data:
+        print(data)
+
+    cursor.close()
+    db.close()
