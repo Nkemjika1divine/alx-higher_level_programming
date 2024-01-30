c575283304fa51747e11501a5c07e81cf8d1433f
Nkemjika1divine <cndivine@gmail.com>
2024-01-30 10:14:14 -0800

    Web scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const args = process.argv;
-const url = 'https://swapi-api.alx-tools.com/api/films/${args[2]}';
+const url = `https://swapi-api.alx-tools.com/api/films/${args[2]}`;
 
 request(url, (err, code, reponse) => {
   if (err) {
