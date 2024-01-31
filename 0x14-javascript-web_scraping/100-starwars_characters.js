8e234e9095477e788364aa14f5a0c0203161da7a
Nkemjika1divine <cndivine@gmail.com>
2024-01-30 18:25:55 -0800

    Web scraping

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+// computes the number of tasks completed by user id
+const request = require('request');
+// const fs = require('fs');
+const args = process.argv;
+const film = args[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${args[2]}`;
+
+request(url, (err, code, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    for (const character of JSON.parse(body).characters) {
+      request(character, (err, code, body) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+});
