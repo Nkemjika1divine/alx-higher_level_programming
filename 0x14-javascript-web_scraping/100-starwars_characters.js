cfc0b471d2cdd8fe4df5dbc223b9eede49563e90
Nkemjika1divine <cndivine@gmail.com>
2024-01-30 18:53:44 -0800

    Web scraping

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,22 +1,16 @@
 #!/usr/bin/node
 
 // prints the list of characters in a movie id passed as argument
-const request = require('request');
-// const fs = require('fs');
 const args = process.argv;
-const url = `https://swapi-api.alx-tools.com/api/films/${args[2]}`;
+const request = require('request');
 
-request(url, (err, code, body) => {
+request('https://swapi-api.alx-tools.com/api/films/' + args[2], (err, code, body) => {
   if (err) {
     console.error(err);
   } else {
     for (const character of JSON.parse(body).characters) {
       request(character, (err, code, body) => {
-        if (err) {
-          console.error(err);
-        } else {
-  i        console.log(JSON.parse(body).name);
-        }
+        if (err) { console.error(err); } else { console.log(JSON.parse(body).name); }
       });
     }
   }
